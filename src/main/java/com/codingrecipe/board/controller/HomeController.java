6a0b8fc633f1afe89d6220d0b097301cd3961e1e
leejunjae00 <wnswo8633@naver.com>
2024-03-06 22:42:14 +0900

    HomeController

--- src/main/java/com/codingrecipe/board/controller/HomeController.java
+++ src/main/java/com/codingrecipe/board/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
   @GetMapping("/")
-  public String index(){
+  public String index(){ //루트 URL에 대한 핸들러, "index" 뷰로 해석해야함
     return "index";
   }
 }
